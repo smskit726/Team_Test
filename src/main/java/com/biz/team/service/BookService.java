a2a6ab9488fbdcde3a76611da66dec5c5977a3e7
smskit726 <smskit726@gmail.com>
2020-08-10 15:08:52 +0900

    Book_CRUD_Start

---
+++ src/main/java/com/biz/team/service/BookService.java
@@ -0,0 +1,10 @@
+package com.biz.team.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+	public void list() {
+		System.out.println("도서 리스트 가져오기");
+	}
+}
